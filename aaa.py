3e2b12fcae22095432cb94173f92278d0da37789
iMikel97 <baranovmihail296@gmail.com>
2022-02-23 22:36:06 +0300

    Y

---
+++ aaa.py
@@ -0,0 +1,34 @@
+from collections import Counter
+
+text = "ddacaacgasdf ad"
+st = 'adcd'
+
+
+def solution(text, st: str):
+    dict = Counter(st)
+    count = len(st)
+    set_st = set(st)
+    j = 0
+    if st == "":
+        return st == text
+    for i in text:
+        if i in set_st:
+            dict[i] -= 1
+            count -= 1
+            j += 1
+            if dict[i] < 0:
+                dict = Counter(st)
+                continue
+        else:
+            dict = Counter(st)
+            count += j
+            j = 0
+        print(count, dict)
+        if count == 0:
+            if sum(dict.values()) == 0:
+                return True
+    return False
+
+
+if __name__ == "__main__":
+    print(solution(text, st))
--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from collections import Counter
 
-text = "adafg ad"
+text = "adafg adc"
 st = 'adc '
 
 
 def solution(text, st: str):
     dict = Counter(st)
-    set_st =set(st)
+    start_dict = Counter(st)
+    set_st = set(st)
     if st == "":
         return st == text
     for i in text:
         if i in set_st:
             dict[i] -= 1
             if dict[i] < 0:
-                dict = Counter(st)
+                dict = start_dict
                 continue
         else:
-            dict = Counter(st)
+            dict = start_dict
         print(dict)
     return sum(dict.values()) == 0
 
